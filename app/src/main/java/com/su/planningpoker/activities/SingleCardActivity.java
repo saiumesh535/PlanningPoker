44a593f4a8c8738235de0d7ecc66a91c7a7c2aa1
saiumesh535 <d.sai535@gmail.com>
2019-12-28 01:39:46 +0530

    added svg list view

---
+++ app/src/main/java/com/su/planningpoker/activities/SingleCardActivity.java
@@ -0,0 +1,14 @@
+package com.su.planningpoker.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_card);
+    }
+}
---
+++ app/src/main/java/com/su/planningpoker/adapters/MainCardsAdapter.java
@@ -0,0 +1,4 @@
+package com.su.planningpoker.adapters;
+
+public class MainCardsAdapter {
+}
