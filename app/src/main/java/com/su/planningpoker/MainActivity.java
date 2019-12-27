a965daee14048753859568301d82df8b115ba2b0
saiumesh535 <d.sai535@gmail.com>
2019-12-27 16:16:07 +0530

    aded avg

--- app/src/main/java/com/su/planningpoker/MainActivity.java
+++ app/src/main/java/com/su/planningpoker/MainActivity.java
@@ -3,12 +3,28 @@ package com.su.planningpoker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hey!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
